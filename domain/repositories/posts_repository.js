12620c16727d301d28b0271bb7c7300c02479b8b
Ahmed Subkhi <ahmed.subkhi@gmail.com>
2018-08-18 01:18:16 +0700

    Update posts repository: Add user detail poster in a post

--- domain/repositories/posts_repository.js
+++ domain/repositories/posts_repository.js
@@ -1,16 +1,32 @@
 var path = require('path');
 var root_dir = process.cwd();
 var Posts = require(path.join(root_dir, 'domain/models/posts'));
+var { Users } = require(path.join(root_dir, 'domain/models/users'));
 
 var repo = module.exports = {
 
   get_all: function(req, res) {
     return new Promise(function(resolve, reject) {
       Posts.find(function (err, data) {
+        var posts = data;
+        var data_post = [];
+
         if (err) {
           reject(err);
         } else {
-          resolve(data);
+          Users.find(function (err, d_user) {
+            posts.map(function(p, i){ // Looping posts data
+              p = p.toObject(); // Convert p from string to Object
+              var usr_index = d_user.findIndex(u => u._id == p.id_user); // Find index of 'users' Collections  depending by id_user that wrote to 'posts' Collections
+              p.user = d_user[usr_index]; // push data user gotten to 'p' variable
+              data_post[i] = p; // push all variable 'p' saved to 'data_post'
+            });
+            if (err) {
+              reject(err);
+            } else {
+              resolve(data_post);
+            }
+          });
         }
       });
     });
@@ -18,11 +34,20 @@ var repo = module.exports = {
 
   get_one: function(id) {
     return new Promise(function(resolve, reject) {
-      Posts.findById(id, function (err, data) {
+      Posts.findOne({ "id_post":id }, function (err, data) {
         if (err) {
           reject(err);
         } else {
-          resolve(data);
+          Users.findById(data.id_user, function (err, d_user) {
+            var data_post = data.toObject(); // Convert 'data' from string to Object
+            data_post.user = d_user;
+            if (err) {
+              reject(err);
+            } else {
+              console.log(data_post);
+              resolve(data_post);
+            }
+          });
         }
       });
     });
